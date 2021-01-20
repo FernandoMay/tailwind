1b4e5d48e54fc4009d8d2d7eb4785a56db0e3d34
Fernando May <fmay130@gmail.com>
2021-01-20 14:56:56 -0600

    one

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      rale: ['Raleway'],
+    },
+    extend: {
+      colors: {
+        danger: '#ff5f40',
+        info: {
+          100: '#024a19c',
+          200: '#6ebfb5'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
